f1ba4fd0871a245842e0f19cfb876aa55091f21e
Apig Sharbo <apigsharbo@gmail.com>
2019-12-10 16:43:00 +0400

    fixed implicit conversion errors for newer versions of gcc

--- Threads/thread.c
+++ Threads/thread.c
@@ -1,8 +1,11 @@
+// run using gcc thread.c -o thread.exe -lpthread
+
 #include <stdio.h>
+#include <unistd.h>
 #include <pthread.h>
 void *mythread1(void * arg){
 	int i;
-	printf("mythread1 arg = %d\n",arg);
+	printf("mythread1 arg = %ld\n",(long)arg);
 	//here we will wait 2s
 	sleep(2);
 	return NULL;
@@ -11,18 +14,17 @@ void *mythread1(void * arg){
 void *mythread2(void * arg){
 	//here we will wait 4s
 	sleep(4);
-	printf("mythread2 arg = %s\n",arg);
+	printf("mythread2 arg = %s\n",(char *)arg);
 	return NULL;
 }
 int main(){
 	pthread_t p1,p2;
-	int k=5;
-	
+    
 	//thread1 is created and starts working
-	pthread_create(&p1,NULL,mythread1,(int*)k);
+	pthread_create(&p1,NULL,mythread1,(int *)5);
 
 	//thread2 is created and is working alongside with thread1
-	pthread_create(&p2,NULL,mythread2,"Hello students!");
+	pthread_create(&p2,NULL,mythread2,(void *)"Hello students!");
 
 	// join is to make the main process wait for thread completion before process termination, NULL is what you expect thread to return.
 	// It is ,i think,different from the return NULL which is mandatory and can't be changed in the body of the thread. 
